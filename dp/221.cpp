4e5356ff8b2a1876a625dd918ce1baaad4f0467f
Martin Morales Arana <mmorale5@lakeheadu.ca>
2026-03-11 13:50:42 -0400

    221 optimal solution. Thought of optimizing space by using the char matrix for tabulation, but char can't store numbers bigger than 255, so that doesn't work. You need a dedicated dp matrix

--- dp/221.cpp
+++ dp/221.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    int expand(vector<vector<char>>& matrix, int i, int o, int level) {
-        int maxLevel = level;
-        while (true) {
-            if (i+maxLevel >= matrix.size() || o+maxLevel >= matrix[0].size())
-                return maxLevel;
-            for (int x=0; x<=maxLevel; x++) {
-                if (matrix[i+maxLevel][o+x] == '0' || matrix[i+x][o+maxLevel]=='0')
-                    return maxLevel;
-            }
-            maxLevel++;
-        }
-    }
-
     int maximalSquare(vector<vector<char>>& matrix) {
         int maxArea = 0;
+        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), 0));
         for (int i=0; i<matrix.size(); i++) {
             for (int o=0; o<matrix[0].size(); o++) {
-                if (matrix[i][o]-'0' != 0) {
-                    if (maxArea>=(matrix.size()-i+1) * (matrix[0].size()-o+1)){
-                        continue;
-                    }
-                    int l = i-1>=0? matrix[i-1][o]-'0': 0;
-                    int u = o-1>=0? matrix[i][o-1]-'0': 0;
-                    int lu = (i-1>=0 && o-1>=0)? matrix[i-1][o-1]-'0':0;
-                    int curMaxLevel = max({l-1, u-1, lu-1, matrix[i][o]-'0'});
-                    int actMaxLevel = expand(matrix, i, o, curMaxLevel);
-                    matrix[i][o] = '0' + actMaxLevel;
-                    maxArea = max(maxArea, actMaxLevel*actMaxLevel);
+                if (!i || !o || matrix[i][o] == '0') {
+                    dp[i][o] = matrix[i][o]-'0';
+                } else {
+                    int l = dp[i-1][o];
+                    int u = dp[i][o-1];
+                    int lu = dp[i-1][o-1];
+
+                    dp[i][o] = min({l, u, lu})+1;
                 }
+                maxArea = max(maxArea, dp[i][o]*dp[i][o]);
             }
         }
         return maxArea;
